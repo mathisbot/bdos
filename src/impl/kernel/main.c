49abc7530997321de4002baa0ac845d1a424954a
Mathis Bottinelli <mathis.bottinelli@student-cs.fr>
2024-02-18 22:45:38 +0100

    Fixed comment

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,5 +4,5 @@
 void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_CYAN, PRINT_COLOR_YELLOW);
-    print_str("Welcome to B-DOS!");
+    print_str("Welcome to B-DOS!\nA simple very simple 64-bit operating system");
 }
