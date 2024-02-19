aaff4ba0724209f725ebcdac6a3b29b3884a535c
Bottinelli Mathis <mathis.bottinelli@student-cs.fr>
2024-02-19 17:58:47 +0100

    Working keyboard input

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,10 +1,40 @@
 #include "print.h"
 
 
+// QWERTY keymap
+struct {
+    unsigned char scancode;
+    char key;
+} keymap[] = {
+    {0x1E, 'A'}, {0x30, 'B'}, {0x2E, 'C'}, {0x20, 'D'}, {0x12, 'E'}, {0x21, 'F'}, {0x22, 'G'}, {0x23, 'H'},
+    {0x17, 'I'}, {0x24, 'J'}, {0x25, 'K'}, {0x26, 'L'}, {0x32, 'M'}, {0x31, 'N'}, {0x18, 'O'}, {0x19, 'P'},
+    {0x10, 'Q'}, {0x13, 'R'}, {0x1F, 'S'}, {0x14, 'T'}, {0x16, 'U'}, {0x2F, 'V'}, {0x11, 'W'}, {0x2D, 'X'},
+    {0x15, 'Y'}, {0x2C, 'Z'}, {0x02, '1'}, {0x03, '2'}, {0x04, '3'}, {0x05, '4'}, {0x06, '5'}, {0x07, '6'},
+    {0x08, '7'}, {0x09, '8'}, {0x0A, '9'}, {0x0B, '0'}, {0x1C, '\n'}, {0x39, ' '}, {0xE, '\b'} // Enter, Space, Backspace
+};
+
+
 void kernel_main() {
 
     // Welcome message
     print_clear();
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_BLACK);
     print_str("Welcome to B-DOS! A very simple 64-bit operating system\nPress any key :\n");
+}
+
+
+int print_key(char scancode) {
+    // Find the key corresponding to the scancode
+    int i;
+    char key = '\0';
+    for (int i=0; i<sizeof(keymap)/sizeof(keymap[0]); i++) {
+        if (keymap[i].scancode == scancode) {
+            key = keymap[i].key;
+            break;
+        }
+    }
+
+    if (key != '\0') print_char(key);
+
+    return 0;
 }
--- src/impl/x86_64/boot/main64.asm
+++ src/impl/x86_64/boot/main64.asm
@@ -1,9 +1,11 @@
 global long_mode_start
 extern kernel_main
-extern print_char  ; Temporary
+extern print_key
 
 section .text
 bits 64
+
+; Entry point for the kernel
 long_mode_start:
     ; Load null into all DS registers
     xor ax, ax
@@ -15,12 +17,33 @@ long_mode_start:
 
 	call kernel_main  ; kernel/main.c
 
-    ; Get keyboard input
-    mov ah, 0  ; Read
-    int 0x16  ; BIOS interrupt
+; Infinite loop to keep the kernel running
+kernel_loop:
+    call check_keyboard_status
+    test eax, eax
+    jz .no_data_available
 
-    push al
-    call print_char  ; kernel/print.c
-    pop al
+    in al, 0x60
+
+    movzx rdi, al
+    call print_key
+
+    jmp kernel_loop
 
     hlt
+
+
+; Handling keyboard input
+.no_data_available:
+    nop
+    jmp kernel_loop
+; EAX = 0 if no data available, 1 if data available
+check_keyboard_status:
+    in al, 0x64
+    test al, 0x01
+    jnz .key_found
+    xor eax, eax
+    ret
+.key_found:
+    mov eax, 1
+    ret
