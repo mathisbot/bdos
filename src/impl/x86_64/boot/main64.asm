4286adee77b426c6b0b8641194eeaa509280f04c
Mathis Bottinelli <mathis.bottinelli@student-cs.fr>
2024-02-19 14:09:27 +0100

    64-bit fix

--- src/impl/x86_64/boot/main64.asm
+++ src/impl/x86_64/boot/main64.asm
@@ -1,11 +1,12 @@
 global long_mode_start
 extern kernel_main
+extern print_char  ; Temporary
 
 section .text
 bits 64
 long_mode_start:
     ; Load null into all DS registers
-    mov ax, 0
+    xor ax, ax
     mov ss, ax
     mov ds, ax
     mov es, ax
@@ -18,8 +19,8 @@ long_mode_start:
     mov ah, 0  ; Read
     int 0x16  ; BIOS interrupt
 
-    push al
+    push rax
     call print_char  ; kernel/print.c
-    pop al
+    pop rax
 
     hlt
